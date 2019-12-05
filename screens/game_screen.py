df21051268ea1eee22536f41ab471bb668f082f2
Hetorus NL <timkleinnijenhuis@hotmail.com>
2019-12-06 00:23:17 +0100

    added snake and reset sequence

--- screens/game_screen.py
+++ screens/game_screen.py
@@ -13,12 +13,15 @@ class GameScreen(Screen):
         self.init = True
         self.pause = False
         self.score = 0
+        self.direction = 0  # [0,1,2,3] == [up,right,down,left]
+        self.snake = [(5, 5), (5, 6), (5, 7)]  # 10 * 13 squares
 
         # load the assets
         self.ready = load("assets/ready.png").convert_alpha()
         self.buttons = load("assets/buttons.png").convert_alpha()
         self.pausemenu = load("assets/pausemenu.png").convert_alpha()
         self.numbers = load("assets/numbers.png").convert_alpha()
+        self.tail = load("assets/tail.png").convert_alpha()
         # load stains
         self.stains = [
             load("assets/stain1.png").convert_alpha(),
@@ -26,16 +29,19 @@ class GameScreen(Screen):
             load("assets/stain3.png").convert_alpha(),
         ]
         # load heads
-        self.headdown = load("assets/headdown.png").convert_alpha()
-        self.headleft = load("assets/headleft.png").convert_alpha()
-        self.headright = load("assets/headright.png").convert_alpha()
-        self.headup = load("assets/headup.png").convert_alpha()
+        self.heads = [
+            load("assets/headup.png").convert_alpha(),
+            load("assets/headright.png").convert_alpha(),
+            load("assets/headdown.png").convert_alpha(),
+            load("assets/headleft.png").convert_alpha(),
+        ]
 
     def draw(self):
         Screen.draw(self)
 
         self.draw_score()
         pygame.draw.line(self.pg_screen, (0, 0, 0), (0, 415), (319, 415))
+        self.draw_snake()
 
         if self.init:
             self.pg_screen.blit(self.ready, (47, 100))
@@ -66,8 +72,26 @@ class GameScreen(Screen):
                 self.numbers, (x_offset, 420), (x_index, 0, 20, 32)
             )
 
+    def draw_snake(self):
+        # draw the tail
+        for tail in self.snake[1:]:
+            self.pg_screen.blit(self.tail, (tail[0] * 32, tail[1] * 32))
+
+        # draw the head (on top)
+        self.pg_screen.blit(
+            self.heads[self.direction],
+            (self.snake[0][0] * 32 - 5, self.snake[0][1] * 32 - 5),
+        )
+
     def update(self, events):
         Screen.update(self, events)
+
+    def reset(self):
+        self.init = True
+        self.pause = False
+        self.direction = 0
+        self.snake = [(5, 5), (5, 6), (5, 7)]
+        self.score = 0
 
     def mouse_down(self, pos):
         if self.init:
@@ -79,16 +103,17 @@ class GameScreen(Screen):
                 self.pause = False
                 return {"play_sound": "click"}
             elif self.pos_between(pos, (90, 151), (233, 190)):
-                self.init = True
-                self.pause = False
+                self.reset()
                 return {"screen": "main_menu", "play_sound": "click"}
         else:  # playing
             if self.pos_between(pos, (0, 0), (64, 64)):
                 self.pause = True
                 return {"play_sound": "click"}
             if self.pos_between(pos, (0, 415), (64, 479)):
+                self.direction = (self.direction - 1) % 4
                 return {"play_sound": "click"}
             if self.pos_between(pos, (256, 415), (320, 479)):
+                self.direction = (self.direction + 1) % 4
                 return {"play_sound": "click"}
 
         # if self.pos_between(pos, (69, 258), (240, 287)):
