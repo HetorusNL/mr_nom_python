fde5a54c7331b8c4010fb2c1aa517c8fb5b19629
Hetorus NL <timkleinnijenhuis@hotmail.com>
2019-12-08 12:10:13 +0100

    fixed bug that allowed key presses when init/pause/gameover

--- screens/game_screen.py
+++ screens/game_screen.py
@@ -123,15 +123,6 @@ class GameScreen(Screen):
             self.pg_screen.blit(self.buttons, (0, 415), (64, 64, 64, 64))
             self.pg_screen.blit(self.buttons, (256, 415), (0, 64, 64, 64))
 
-        # draw the main menu
-        # self.pg_screen.blit(self.logo, (5, 60))
-        # self.pg_screen.blit(self.register_login, (150, 5))
-        # self.pg_screen.blit(self.main_menu, (60, 250))
-        # s = 64 - self.sound_on * 64
-        # self.pg_screen.blit(self.buttons, (0, 415), (s, 0, 64, 64))
-        # m = 64 - self.music_on * 64
-        # self.pg_screen.blit(self.buttons, (64, 415), (m, 192, 64, 64))
-
     def draw_score(self):
         score_length = len(str(self.score))
         for i in range(score_length):
@@ -188,20 +179,11 @@ class GameScreen(Screen):
                 self.direction = (self.direction - 1) % 4
                 return {"play_sound": "click"}
 
-        # if self.pos_between(pos, (69, 258), (240, 287)):
-        #    return {"screen": "game_screen", "play_sound": "click"}
-        # elif self.pos_between(pos, (71, 301), (243, 330)):
-        #    return {"screen": "highscores_screen", "play_sound": "click"}
-        # elif self.pos_between(pos, (72, 345), (242, 379)):
-        #    return {"screen": "help_screen", "play_sound": "click"}
-        # elif self.pos_between(pos, (0, 415), (64, 479)):
-        #    self.sound_on = not self.sound_on
-        #    return {"sound": self.sound_on, "play_sound": "click"}
-        # elif self.pos_between(pos, (64, 415), (128, 479)):
-        #    self.music_on = not self.music_on
-        #    return {"music": self.music_on, "play_sound": "click"}
-
     def key_press(self, direction):
+        if self.init or self.pause or self.game_over:
+            return
+
+        # user is playing, handle key press
         self.direction = direction
 
     def _generate_stain_pos(self):
